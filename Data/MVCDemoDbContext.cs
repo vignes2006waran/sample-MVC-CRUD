a475548a0fa7e0319775e79681897887c2edeba0
vignes2006waran <vigneswaran2830s@gmail.com>
2023-09-14 21:16:33 +0530

    Add project files.

---
+++ Data/MVCDemoDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.DataProtection;
+using Microsoft.EntityFrameworkCore;
+using sample_MVC_CRUD.Models.Domain;
+
+namespace sample_MVC_CRUD.Data
+{
+    public class MVCDemoDbContext : DbContext
+    {
+        public MVCDemoDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
